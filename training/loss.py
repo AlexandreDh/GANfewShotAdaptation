b73fff42dae2741cf1c01566d23ed3fa2e0d478f
Alexandre D <alexandredehlavi@hotmail.com>
2022-06-29 15:37:39 +0200

    Loss unpack correctly generator mapping function

--- training/loss.py
+++ training/loss.py
@@ -46,7 +46,7 @@ class StyleGAN2Loss(Loss):
                     cutoff = torch.where(torch.rand([], device=ws.device) < self.style_mixing_prob, cutoff, torch.full_like(cutoff, ws.shape[1]))
                     ws[:, cutoff:] = self.G_mapping(torch.randn_like(z), c, skip_w_avg_update=True)[:, cutoff:]
         with misc.ddp_sync(self.G_synthesis, sync):
-            img = self.G_synthesis(ws)
+            img, _ = self.G_synthesis(ws)
         return img, ws
 
     def run_D(self, img, c, sync):
