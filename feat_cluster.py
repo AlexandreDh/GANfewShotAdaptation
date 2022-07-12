Try to fix double gradient descend for Discriminator contrastive loss

feat_cluster.py
@@ -4,6 +4,7 @@ import random
 import PIL
 import torch
 import lpips
+from tqdm import tqdm
 from torchvision import transforms
 import os
 import dnnlib
@@ -52,8 +53,15 @@ def assign_to_cluster_centers(n_samples, outdir, center_folder, network_pkl, see
     generated_dir = os.path.join(outdir, "generated")
     os.makedirs(generated_dir, exist_ok=True)
 
+    num_iter =  n_samples // batch_size + 1 if n_samples % batch_size > 0 else 0
+    pbar = tqdm(num_iter)
     with torch.no_grad():
-        for idx, (z, c) in enumerate(zip(all_z, all_c)):
+        for idx in pbar:
+            start = idx * batch_size
+            end = (idx + 1) * batch_size
+            z = all_z[start:end]
+            c = all_c[start:end]
+
             images = G(z, c)
             images = (images.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
 

training/loss.py
@@ -11,6 +11,7 @@ import torch
 from torch_utils import training_stats
 from torch_utils import misc
 from torch_utils.ops import conv2d_gradfix
+import tqdm
 
 
 # ----------------------------------------------------------------------------
@@ -430,4 +431,9 @@ class FewShotAdaptationLoss(Loss):
                     training_stats.report('Loss/D/reg', loss_Dr1)
 
             with torch.autograd.profiler.record_function(name + '_backward'):
-                (real_logits * 0 + torch.cat(feats_real, dim=0) * 0 + loss_Dreal + loss_Dr1 + loss_D_diversity).mean().mul(gain).backward()
+                sum_feats = 0
+                if adaptation == "DCL":
+                    for f in feats_real:
+                        sum_feats += f * 0
+
+                (real_logits * 0 + sum_feats + loss_Dreal + loss_Dr1 + loss_D_diversity).mean().mul(gain).backward()